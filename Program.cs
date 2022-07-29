21635b6245bf44788fd1bd92b8918c66d7811990
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-29 10:01:05 +0530

    Wages based on working hours and days

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace EmployeeWageComputation
+﻿using EmployeeWage;
+
+namespace EmployeeWageComputation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage program.");
+
+
+            //CheckEmployee.CheckEmployeeAttendence();
+            WageOfWorkingHour.WageOfTotalWorkingHr();
         }
     }
 }
