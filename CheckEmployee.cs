08d77f2af31989d1f6f60e57863d6e1b308a622e
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-29 09:24:05 +0530

    Using switch case

--- CheckEmployee.cs
+++ CheckEmployee.cs
@@ -17,33 +17,34 @@ namespace EmployeeWage
         public static void CheckEmployeeAttendence()
         {
             //Constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int WAGE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
             //Variables
             int empHr = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(3);
+                    Random random = new Random();
+                    int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Present");
-                empHr = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee did Parttime.");
-                empHr = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHr = 0;
-            }
-             empWage = empHr * WAGE_PER_HOUR;
-            Console.WriteLine("Employee wage is: " + empWage);
+                switch(empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empWage = empHr * WAGE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is: " + empWage);
+           
         }
     }
 }
